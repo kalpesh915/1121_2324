5ae9bd74eababa6ca3d65015164c0e5ed0109eba
Kalpesh Chauhan <63454815+kalpesh915@users.noreply.github.com>
2024-03-20 12:27:21 +0530

    PHP
    Introduction Day 1

---
+++ PHP/pro3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $username = "Alan Thomas";
+        echo "Welcome $username to PHP";  
+    ?>
+</body>
+</html>
